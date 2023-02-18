0a6bd8d446bc89fc7800cab53de6c26e054411d2
BautistaMendibe <bautistamendibe10@gmail.com>
2023-02-18 13:26:49 -0300

    avances

---
+++ frontend/Dallie-Clone/src/components/FormField.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export class FormField extends Component {
+  render() {
+    return (
+      <div>FormField</div>
+    )
+  }
+}
+
+export default FormField
+
